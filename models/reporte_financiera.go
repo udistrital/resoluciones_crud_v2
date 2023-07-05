10d3652dd0d0d8d2243d3cfa47bfbe2cf6b5ac12
Diego Gutierrez <diagutierrezro@unal.edu.co>
2023-07-05 12:38:01 -0500

    fix: Ajuste de consulta para reporte financiera

--- models/reporte_financiera.go
+++ models/reporte_financiera.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/astaxie/beego/orm"
 )
@@ -13,6 +14,7 @@ type ReporteFinanciera struct {
 	Horas                 int     `orm:"column(horas);null"`
 	Semanas               int     `orm:"column(semanas);null"`
 	Total                 float64 `orm:"column(total);null"`
+	Cdp                   int     `orm:"column(cdp);null"`
 	SueldoBasico          float64 `orm:"column(sueldobasico);null"`
 	PrimaNavidad          float64 `orm:"column(primanavidad);null"`
 	Vacaciones            float64 `orm:"column(vacaciones);null"`
@@ -33,67 +35,30 @@ func ReporteFinancieraQuery(m *DatosReporte) (reporte []ReporteFinanciera, err e
 
 	fmt.Println("m ", m)
 
-	query := `
-	SELECT r.id, r.numero_resolucion as resolucion, 
-	v.persona_id as cedula,
-	SUM(v.numero_horas_semanales) as Horas,
-	r.numero_semanas as Semanas,
-	SUM(v.valor_contrato) as Total,
-	sum(rubros.SueldoBasico) as SueldoBasico,
-	sum(rubros.PrimaNavidad) as PrimaNavidad,
-	sum(rubros.Vacaciones) as Vacaciones,
-	sum(rubros.PrimaVacaciones) as PrimaVacaciones,
-	sum(rubros.Cesantias) as Cesantias,
-	sum(rubros.InteresesCesantias) as InteresesCesantias,
-	sum(rubros.PrimaServicios) as PrimaServicios,
-	sum(rubros.BonificacionServicios) as BonificacionServicios
-FROM resoluciones_new.resolucion r, resoluciones_new.resolucion_vinculacion_docente rv, resoluciones_new.resolucion_estado re, resoluciones_new.vinculacion_docente v, (
-	SELECT * FROM crosstab(
-	'SELECT vinculacion_docente_id, rubro, valor FROM resoluciones_new.disponibilidad_vinculacion ORDER BY vinculacion_docente_id DESC',
-	'SELECT DISTINCT(rubro) FROM resoluciones_new.disponibilidad_vinculacion AS rubro'
-) AS (
-	vinculacion_id NUMERIC(10,0),
-	BonificacionServicios NUMERIC(16,3),
-	PrimaVacaciones NUMERIC(16,3),
-	PrimaNavidad NUMERIC(16,3),
-	Cesantias NUMERIC(16,3),
-	SueldoBasico NUMERIC(16,3),
-	Vacaciones NUMERIC(16,3),
-	InteresesCesantias NUMERIC(16,3),
-	PrimaServicios NUMERIC(16,3)
-	)
- ) rubros WHERE rubros.vinculacion_id=v.id
-AND r.id = rv.id AND rv.id = v.resolucion_vinculacion_docente_id AND r.id = re.resolucion_id
-AND r.dependencia_id=14 AND rv.nivel_academico='PREGRADO'
-AND r.tipo_resolucion_id=583 
-AND re.estado_resolucion_id=602
-GROUP BY r.id, r.numero_resolucion, v.persona_id, r.numero_semanas
-ORDER BY r.id DESC;`
-	/*
-				SELECT r.id, r.numero_resolucion as resolucion,
-		                v.persona_id as cedula,
-		                SUM(v.numero_horas_semanales) as Horas,
-		                r.numero_semanas as Semanas,
-		                SUM(v.valor_contrato) as Total,
-						SUM(dv.valor) filter (WHERE dv.rubro='SueldoBasico') as sueldobasico,
-						SUM(dv.valor) filter (WHERE dv.rubro='PrimaNavidad') as primanavidad,
-						SUM(dv.valor) filter (WHERE dv.rubro='Vacaciones') as vacaciones,
-						SUM(dv.valor) filter (WHERE dv.rubro='PrimaVacaciones') as primavacaciones,
-						SUM(dv.valor) filter (WHERE dv.rubro='Cesantias') as cesantias,
-						SUM(dv.valor) filter (WHERE dv.rubro='InteresesCesantias') as interesescesantias,
-						SUM(dv.valor) filter (WHERE dv.rubro='PrimaServicios') as primaservicios,
-						SUM(dv.valor) filter (WHERE dv.rubro='BonificacionServicios') as bonificacionservicios
-		        FROM resoluciones_new.resolucion r, resoluciones_new.resolucion_vinculacion_docente rv, resoluciones_new.resolucion_estado re, resoluciones_new.vinculacion_docente v, resoluciones_new.disponibilidad_vinculacion dv
-		        WHERE
-		                r.id = rv.id AND rv.id = v.resolucion_vinculacion_docente_id AND r.id = re.resolucion_id AND v.id=dv.vinculacion_docente_id
-		                AND r.dependencia_id=14 AND rv.nivel_academico='POSGRADO'
-		                AND r.numero_resolucion='2707'
-		                AND r.tipo_resolucion_id=583
-		                AND re.estado_resolucion_id=602
-		        GROUP BY r.id, r.numero_resolucion, v.persona_id, r.numero_semanas
-		        ORDER BY r.id DESC;*/
-	fmt.Println("query ", query)
+	query :=
+		`SELECT r.id, r.numero_resolucion as resolucion,
+			v.persona_id as cedula,
+			SUM(DISTINCT v.numero_horas_semanales) as Horas,
+			r.numero_semanas as Semanas,
+			SUM(DISTINCT v.valor_contrato) as Total,
+			SUM(DISTINCT dv.disponibilidad) as cdp,
+			SUM(dv.valor) filter (WHERE dv.rubro='SueldoBasico') as sueldobasico,
+			SUM(dv.valor) filter (WHERE dv.rubro='PrimaNavidad') as primanavidad,
+			SUM(dv.valor) filter (WHERE dv.rubro='Vacaciones') as vacaciones,
+			SUM(dv.valor) filter (WHERE dv.rubro='PrimaVacaciones') as primavacaciones,
+			SUM(dv.valor) filter (WHERE dv.rubro='Cesantias') as cesantias,
+			SUM(dv.valor) filter (WHERE dv.rubro='InteresesCesantias') as interesescesantias,
+			SUM(dv.valor) filter (WHERE dv.rubro='PrimaServicios') as primaservicios,
+			SUM(dv.valor) filter (WHERE dv.rubro='BonificacionServicios') as bonificacionservicios
+		FROM resoluciones_new.resolucion r, resoluciones_new.resolucion_vinculacion_docente rv, resoluciones_new.resolucion_estado re, resoluciones_new.vinculacion_docente v, resoluciones_new.disponibilidad_vinculacion dv
+		WHERE
+			r.id = rv.id AND rv.id = v.resolucion_vinculacion_docente_id AND r.id = re.resolucion_id AND v.id=dv.vinculacion_docente_id
+			AND r.dependencia_id=` + strconv.Itoa(m.Facultad) + `AND rv.nivel_academico='` + m.NivelAcademico + `'
+			AND r.numero_resolucion='` + m.Resolucion + `' AND r.vigencia=` + strconv.Itoa(m.Vigencia) + `
+			AND r.tipo_resolucion_id=583
+			AND re.estado_resolucion_id=602
+		GROUP BY r.id, r.numero_resolucion, v.persona_id, r.numero_semanas
+		ORDER BY r.id DESC;`
 	_, err = o.Raw(query).QueryRows(&reporte)
-	fmt.Println(reporte)
 	return reporte, nil
 }
